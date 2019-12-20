3eb55a440b951f104f13708ea79202dc4fce1e23
lschimm <lschimmelfennig@gmail.com>
2019-12-20 12:50:31 -0600

    Adds notes to queues

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,9 +1,11 @@
 import sys
-sys.path.append('../doubly_linked_list')
+sys.path.append('./doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 # Queues are first in, first out
 # insert, delete/ enqueue, dequeue
+# new things ADDED/ENQUEUED to tail
+# REMOVES/DEQUEUES from the front
 class Queue:
     def __init__(self):
         self.size = 0
@@ -15,14 +17,11 @@ class Queue:
         self.storage.add_to_head(value)
 
     def dequeue(self):
-        if self.size == 0:
-            return None
-        # if self.size > 0:
-        #     return self.size -= 1 # not needed since code below takes care of this
-        else:
+        if self.size <= 0: # if the size/length is == 0
+            return None    # return None
+        if self.size > 0:
             self.size = self.size - 1
             return self.storage.remove_from_tail()
 
     def len(self):
-        pass
-    
+        return self.size
