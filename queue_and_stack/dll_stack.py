e53a2e04f0603f6d0fc382a6cb40a36a6f417016
lschimm <lschimmelfennig@gmail.com>
2019-12-20 12:54:55 -0600

    Adds notes for Stack

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,6 +2,12 @@ from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
 
+## Stack is Last in, first out
+#    ___
+#   _____
+#  ________
+# it brings/PUSHES items up and items come/POP off  the top
+
 class Stack:
     def __init__(self):
         self.size = 0
